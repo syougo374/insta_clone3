311776ad85a4126685f9b6326fc77c815d224260
syougo374 <supernaturalsyougo@gmail.com>
2021-04-25 17:35:17 +0900

    お気に入り用モデル作成

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,5 +1,6 @@
 class Picture < ApplicationRecord
   belongs_to :user
+  has_many :favorites, dependent: :destroy
   validates :content, presence: true
   validates :image, presence: true
   mount_uploader :image, ImageUploader
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,12 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   before_validation { email.downcase! }
   has_many :pictures
+  has_secure_password
+  has_many :favorites, dependent: :destrou
+  has_many :favorite_users, through: :favorites, source: :picture
+
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-
-  has_secure_password
+  has_many :favorites, dependent: :destroy
   validates :password, presence: true, length: { minimum: 6 }
 end
