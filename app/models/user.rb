0d84afb9821cc6b6fd7347a24237af2306ff7e4a
syougo374 <supernaturalsyougo@gmail.com>
2021-04-25 21:29:17 +0900

    mailer追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,8 @@ class User < ApplicationRecord
   before_validation { email.downcase! }
   has_many :pictures
   has_secure_password
-  has_many :favorites, dependent: :destrou
-  has_many :favorite_users, through: :favorites, source: :picture
-
+  has_many :favorites, dependent: :destroy
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }, uniqueness: true
-  has_many :favorites, dependent: :destroy
   validates :password, presence: true, length: { minimum: 6 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
   resources :sessions, only: [:new, :create, :destroy]
   resources :users, only: [:index, :new, :create, :edit, :update, :show]
+  resources :favorites, only: [:create, :destroy, :show]
   root to: 'sessions#new'
   resources :pictures do
     collection do
       post :confirm
     end
   end
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
